013c89a58a3a3f4794bb982585248aec8c8c30d6
Gabriel-Batista <gabedbatista@gmail.com>
2018-09-23 20:12:40 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,62 +5,62 @@ import ToppingForm from './ToppingForm'
 import SideForm from './SideForm'
 
 const DEFAULT_STATE = {
-  protein: [],
-  fillings: [],
-  toppings: [],
-  sides: []
+    protein: [],
+    fillings: [],
+    toppings: [],
+    sides: []
 }
 
 class Form extends Component {
-  state = {
-    ...DEFAULT_STATE
-  }
+    state = {
+        ...DEFAULT_STATE
+    }
 
-  handleSubmit() {
-    event.preventDefault()
-    document.getElementById("order-form").reset()
-    this.props.addOrder(this.state)
-    this.setState({
-      ...DEFAULT_STATE
-    })
-  }
+    handleSubmit= (event) => {
+        event.preventDefault()
+        document.getElementById("order-form").reset()
+        this.props.addOrder(this.state)
+        this.setState({
+            ...DEFAULT_STATE
+        })
+    }
 
-  handleChange() {
-    const itemType = event.target.name
-    const item = event.target.value
+    handleChange= (event) => {
+        const itemType = event.target.name
+        const item = event.target.value
 
-    !this.state[`${itemType}`].includes(item) ?
-      this.setState({
-        [itemType]: this.state[`${itemType}`].concat(item)
-      })
-    :
-      this.setState({
-        [itemType]: this.state[`${itemType}`].filter(
-          ingr => ingr !== item
+        !this.state[`${itemType}`].includes(item) ?
+            this.setState({
+                [itemType]: this.state[`${itemType}`].concat(item)
+            })
+            :
+            this.setState({
+                [itemType]: this.state[`${itemType}`].filter(
+                    ingr => ingr !== item
+                )
+            })
+    }
+
+    render() {
+        return (
+            <div className="ui raised container segment">
+                <h1 className="ui block header">Order Form</h1>
+                <form className="ui form" id="order-form" onSubmit={this.handleSubmit}>
+                    <ProteinForm handleChange={this.handleChange} />
+
+                    <FillingForm handleChange={this.handleChange} />
+
+                    <ToppingForm handleChange={this.handleChange} />
+
+                    <SideForm handleChange={this.handleChange} />
+
+                    <br />
+
+                    <button className="ui blue big button" type="submit">Submit</button>
+                </form>
+            </div>
         )
-      })
-  }
-
-  render() {
-    return(
-      <div className="ui raised container segment">
-        <h1 className="ui block header">Order Form</h1>
-        <form className="ui form" id="order-form" onSubmit={this.handleSubmit}>
-          <ProteinForm handleOnChange={this.handleChange} />
-
-          <FillingForm handleOnChange={this.handleChange} />
-
-          <ToppingForm handleOnChange={this.handleChange} />
-
-          <SideForm handleOnChange={this.handleChange} />
-
-          <br />
-
-          <button className="ui blue big button" type="submit">Submit</button>
-        </form>
-      </div>
-    )
-  }
+    }
 }
 
 export default Form
